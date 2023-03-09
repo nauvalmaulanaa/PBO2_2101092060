f3b1447342e29c447a1f2eb278b1f4449c069cdc
Nauval Maulana <nauvaljr2002@gmail.com>
2023-03-09 16:00:50 +0700

    Create Anggota.java

---
+++ PBO2_2101092060/src/nauval/model/Anggota.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval.model;
+
+/**
+ *
+ * @author Nauval
+ */
+public class Anggota {
+    private String kodeAnggota;
+    private String namaAnggota;
+    private String alamat;
+    private String jenisKelamin;
+    
+    public Anggota(){    
+    }
+
+    public Anggota(String kodeAnggota, String namaAnggota, String alamat, String jenisKelamin) {
+        this.kodeAnggota = kodeAnggota;
+        this.namaAnggota = namaAnggota;
+        this.alamat = alamat;
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+
+    public String getKodeAnggota() {
+        return kodeAnggota;
+    }
+
+    public String getNamaAnggota() {
+        return namaAnggota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getJenisKelamin() {
+        return jenisKelamin;
+    }
+
+    public void setKodeAnggota(String kodeAnggota) {
+        this.kodeAnggota = kodeAnggota;
+    }
+
+    public void setNamaAnggota(String namaAnggota) {
+        this.namaAnggota = namaAnggota;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public void setJenisKelamin(String jenisKelamin) {
+        this.jenisKelamin = jenisKelamin;
+    }
+    
+    
+}
