2426807e9a8e863ceb55d98dc0bd90a9c95dfe73
Nauval Maulana <nauvaljr2002@gmail.com>
2023-07-24 23:28:13 +0700

    Create Koneksi.java

---
+++ PBO2_2101092060/src/nauval/dao/Koneksi.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nauval.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author User
+ */
+public class Koneksi {
+    private String url = "jdbc:mysql://localhost/pbo2_2101092060";
+    private String username = "root";
+    private String password = "";
+    
+    public Connection getKoneksi() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(url, username, password);
+    }
+
+   
+}
+
